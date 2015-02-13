50cc22d6e54b5f1d5a4e69d8f60bc7ff6906a990
yjw <wsklxsx@126.com>
2015-02-14 07:30:49 +0800

    change to student_id
    for the password shouldn’t be remembered, so i didn’t add memory for
    the register3

--- app/src/main/java/com/learn2develop/yueme/yuemebeta1/Register5.java
+++ app/src/main/java/com/learn2develop/yueme/yuemebeta1/Register5.java
@@ -4,14 +4,21 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class Register5 extends Activity {
-
+    static String student_id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register5);
+
+        TextView v = (TextView)findViewById(R.id.editText1);
+        student_id = v.getText().toString();
+        if(student_id != null){
+            v.setText(student_id);
+        }
     }
 
     public void onClickone(View view)
