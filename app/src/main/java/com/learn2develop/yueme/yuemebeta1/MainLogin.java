1c3745bb64dd1f8dbe07fd321db9f115541aa20c
tangyuhao <yhtang1993@126.com>
2015-02-13 10:29:06 +0800

    update by lvchenhao

---
+++ app/src/main/java/com/learn2develop/yueme/yuemebeta1/MainLogin.java
@@ -0,0 +1,37 @@
+package com.learn2develop.yueme.yuemebeta1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.avos.avoscloud.AVOSCloud;
+import com.avos.avoscloud.AVAnalytics;
+
+
+public class MainLogin extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_login);
+        AVOSCloud.initialize(this, "n0q7bdc0vg1tb188z4evy0khzto6a32hos7ycadjpgd26bpc", "drgsp2o75vfru4ifet8rx2gw3lmqyu9sdp6ijjpo15bpwem8");
+        AVAnalytics.trackAppOpened(getIntent());
+    }
+
+    public void onClickone(View view)
+    {
+        //检查登入成功则进入Mainisland
+        startActivity(new Intent(this,Mainisland.class));
+    }
+
+    public void onClicktwo(View view)
+    {
+
+    }
+
+    public void onClickthree(View view)
+    {
+        startActivity(new Intent(this,Register1.class));
+    }
+}
---
+++ app/src/main/java/com/learn2develop/yueme/yuemebeta1/Mainisland.java
@@ -0,0 +1,20 @@
+package com.learn2develop.yueme.yuemebeta1;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class Mainisland extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainisland);
+    }
+
+
+
+}
